d56faa995fb778467fb5de98943b47a5e1538f0a
Arie <arieinjector@gmail.com>
2023-01-20 20:48:35 +0700

    add 404 page & some changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react"
+
 const Nav = () => {
+    
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            document.querySelector('nav').classList.toggle('bg-[#ffffff25]', window.scrollY > 0)
+            document.querySelector('nav').classList.toggle('py-4', window.scrollY > 0)
+        })
+    })
+
     return (
-        <nav className="text-white flex items-center justify-around font-notosans py-10">
+        <nav className="text-white flex items-center fixed w-full z-10 justify-around font-notosans py-10 transition-all duration-300">
             <div className="flex items-center">
                 <img src="/assets/icons/icon.png" />
                 <p className="font-dmsans font-bold text-2xl ml-4">Collosal.</p>
